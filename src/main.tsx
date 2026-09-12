8408b4381065ae1de4252c0e146b1aca0aab87e7
Zaki2707 <167660146+Zaki2707@users.noreply.github.com>
2026-09-12 08:28:42 +0700

    fix: import Modul Ajar 2 in app entry

--- src/main.tsx
+++ src/main.tsx
@@ -21,6 +21,7 @@ import './adminModules.js';
 import './cbtModules.js';
 import './pedagogyNonAIEngine.js';
 import './modulAjarModule.js';
+import './modulAjar2Module.js';
 import './settingsAndMisc.js';
 import './themePackageModule.js';
 import './themeEngineV2.js';
